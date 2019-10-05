b60b1641a07c8c885ef31a32a857567311492a11
Fidel Guajardo <fidel.guajardo@harlandclarke.com>
2019-10-05 17:19:41 -0500

    L6: lifecycle hooks

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,18 @@ class _MyFirstWidgetState extends State<MyFirstWidget> {
   int count = 0;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+  
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
