07638f44de694771b93dc309587ee6b3fcb9f821
Fidel Guajardo <fidel.guajardo@harlandclarke.com>
2019-10-07 21:18:24 -0500

    L7: Plaform, Cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+//import 'dart:io' show Platform;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: MyFirstWidget(color: Colors.amber)
-    ),
-    );
-  }
-}
-
-class MyFirstWidget extends StatefulWidget {
-
-  final Color color;
-
-  MyFirstWidget({ Key key, this.color: Colors.red }): super(key: key);
-
-  @override
-  _MyFirstWidgetState createState() => _MyFirstWidgetState();
-}
-
-// class _MyFirstWidgetState extends State<MyFirstWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Container(
-//       width: 50,
-//       height: 50,
-//       color: widget.color,
-//     ),
-//     );
-//   }
-// }
-
-class _MyFirstWidgetState extends State<MyFirstWidget> {
-
-  int count = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-        child: Text('$count'),
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-        },
-    ),
+        body: Center(
+          child: Switch(value: true, onChanged: (v) => null),
+        ),
+      ),
     );
   }
 }
