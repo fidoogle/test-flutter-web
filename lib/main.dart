b0a4a58ac0f75406000a05cc2c303846f7432e87
Fidel Guajardo <fidel.guajardo@harlandclarke.com>
2019-10-08 21:28:05 -0500

    L11: wrap text with Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Row(
             children: <Widget>[
-              Expanded(
+              Expanded(//make text wrap
                 child: Container(
                   height: 100,
                   child: Text(
-                    'Hello World this is too long',
-                    overflow: TextOverflow.ellipsis,
+                    'Hello World this is too long. Use Provider Finder® to search for doctors and hospitals in your plan network. You can also estimate costs for a medical service. Always confirm that the provider you choose is in your plan network to make sure the service is covered.',
+                    //overflow: TextOverflow.ellipsis,
                     style: TextStyle(
-                      fontSize: 50,
+                      fontSize: 14,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
